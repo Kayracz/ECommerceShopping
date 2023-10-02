b09cc9ebb1e16657c7bd72f5fae634fdaf3d9c5e
Kayra Czerniewicz T <31418061+Kayracz@users.noreply.github.com>
2023-10-02 13:35:03 -0400

    deleted unusued main attributes and new orders

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './Pages/App/index.jsx'
-// import Layout from './Components/Layout/index.jsx'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     {/* <Layout> */}
     <App />
-    {/* </Layout> */}
   </React.StrictMode>,
 )
