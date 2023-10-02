b5b6be5090e541911c649d508fc8710ff269310f
Kayra Czerniewicz T <31418061+Kayracz@users.noreply.github.com>
2023-10-02 13:40:31 -0400

    changed vite configuration so that vite reads main.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // Import the 'path' module
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Specify the entry point
+  resolve: {
+    alias: {
+      // You can create an alias for your main.jsx file
+      // Adjust the path based on your project structure
+      '@': path.resolve(__dirname, 'src/main.jsx'),
+    },
+  },
 })
